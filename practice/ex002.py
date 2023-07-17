76e362ba7b85dfac2810b583afd301251c38dfef
zhengyuan.0222 <zhengyuan.0222@bytedance.com>
2023-07-17 17:21:39 +0800

    format; 补充：max() 如果存在多个最大的，默认返回第一个找到的

--- practice/ex002.py
+++ practice/ex002.py
@@ -16,9 +16,10 @@ print(high('take me to semynak') == 'semynak')
 # 三个均为True，则通过。
 """
 
+
 # 小编解法
 def high(x):
-    return sorted([i for i in x.split()], key=lambda y:sum(ord(j)-96 for j in y), reverse=True)[0]
+    return sorted([i for i in x.split()], key=lambda y: sum(ord(j) - 96 for j in y), reverse=True)[0]
 
 
 # 目前最佳解法
@@ -31,20 +32,19 @@ if __name__ == '__main__':
     print(high('what time are we climbing up the volcano') == 'volcano')
     print(high('take me to semynak') == 'semynak')
 
-
-	
 """
 知识点：
 1.获得字母的ASCII码值，ord()
-	ord('a')
-	>>>97
+    ord('a')
+    >>>97
 2.将一句话切割成一个列表，不需要列表生成式
-	s = 'man i need a taxi up to ubud'
-	li = [x for x in s.split()]
-	>>>['man', 'i', 'need', 'a', 'taxi', 'up', 'to', 'ubud']
-	其实，s.split()已经完成工作了。
-	s.split()
-	>>>['man', 'i', 'need', 'a', 'taxi', 'up', 'to', 'ubud']
+    s = 'man i need a taxi up to ubud'
+    li = [x for x in s.split()]
+    >>>['man', 'i', 'need', 'a', 'taxi', 'up', 'to', 'ubud']
+    其实，s.split()已经完成工作了。
+    s.split()
+    >>>['man', 'i', 'need', 'a', 'taxi', 'up', 'to', 'ubud']
 3.max()方法也可以传key
-	将一个匿名方法传进，形成规则。
+    将一个匿名方法传进，形成规则。
+4.如果存在多个最大的，max()默认返回第一个找到的
 """
